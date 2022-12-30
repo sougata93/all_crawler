09db40e9892cdaaeea14426da36cdd194bcbcae0
Sougata Mahata <sougatamahata93@gmail.com>
2022-12-30 10:29:17 +0530

    linux update

--- careindia.py
+++ careindia.py
@@ -5,7 +5,8 @@ import scrapy
 from scrapy.selector import Selector
 import re
 import os
-
+from playwright.async_api import async_playwright
+import asyncio
 url="https://www.careinsurance.com/public-disclosures.html"
 domain="https://www.careinsurance.com"
 
@@ -19,24 +20,51 @@ year=['2021-22','2022-23']
 
 quarter={'q1':'1st Quarter','q2':'2nd Quarter','q3':'3rd Quarter','q4':'4th Quarter'}
 
+
+async def run(FY):
+    async with async_playwright() as pw:
+        browser = await pw.chromium.launch()
+        context = await browser.new_context()
+        page = await context.new_page()
+        await page.goto(url)
+        print(FY)
+        # Click text=FY 21-22
+        await page.frame_locator("#iFrameResizer0").locator("text=FY "+FY).click()
+        # await page.frame_locator("#iFrameResizer0").locator("text=Qtr 1 22-23").click()
+        
+        return await page.content()
+
+
+
+
 def yearFormat(year):
-    year='20'+year
     year=re.sub('_','-',year)
     return year
 
 def FYQ(year,Q):
-    print(res_n)
-    t=res_n[0].css('brochureQuestion')
-    print(t)
     
-    for x in res_n[0].xpath(".//div[@class='sectionRight']"):
-        print(x)
-        qText=x.xpath(".//div[@class='leftotherbox']//div[@class='jss297 jss1040 jss1010 jss1014']")
-        print(qText)
+    data=asyncio.run(run(yearFormat(year)))
+    # print(data)
+
+    if data==None:
+        return
+    soup=BeautifulSoup(data,'lxml')
+    # x=soup.find('div',class_='brochureOuter')
+    x=soup.find_all('a')
+    for i in x:
+        nlText=i.text
+        if nlText!=None:
+            
+            if '.pdf' in nlText:
+                print(nlText)
+
+
+        # qText=x.xpath(".//div[@class='leftotherbox']//div[@class='jss297 jss1040 jss1010 jss1014']")
+        # print(qText)
         
     
 
-FYQ('22_23','q2')
+FYQ('21_22','q2')
 
 
         
